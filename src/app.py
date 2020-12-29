35bc350861a9aa36d1f06223591e73ed151d4a48
rodrigomelo <melorodrigo@gmail.com>
2020-12-29 12:07:06 -0300

    functional version

---
+++ src/app.py
@@ -0,0 +1,108 @@
+import logging
+import traceback
+from flask import Flask, Blueprint
+from flask_restplus import Api, Resource, fields
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm.exc import NoResultFound
+
+logging.basicConfig(level=logging.INFO)
+handler = logging.FileHandler("my-investments-api.log")
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+log.addHandler(handler)
+
+app = Flask("my-investments-api")
+app.config['RESTPLUS_VALIDATE'] = True
+app.config['ERROR_404_HELP'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy()
+db.init_app(app)
+db.create_all(app=app)
+
+blueprint = Blueprint('api', __name__, url_prefix='/api')
+
+api = Api(version='1.0', title='my-investments-api')
+
+api.init_app(blueprint)
+app.register_blueprint(blueprint)
+
+
+@api.errorhandler
+def default_error_handler(e):
+    message = 'An unhandled exception occurred.'
+    log.exception(message)
+    return {'message': message}, 500
+
+
+@api.errorhandler(NoResultFound)
+def database_not_found_error_handler(e):
+    log.warning(traceback.format_exc())
+    return {'message': 'A database result was required but none was found.'}, 404
+
+
+ns_default = api.default_namespace
+
+
+@ns_default.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+api.add_namespace(ns_default)
+
+
+class Trader(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(64), nullable=False)
+    last_name = db.Column(db.String(64), nullable=False)
+
+
+trader_fields = api.model('Trader', {
+    'id': fields.Integer(readonly=True),
+    'first_name': fields.String(required=True),
+    'last_name': fields.String(required=True)
+})
+
+ns_trader = api.namespace('traders', escription='Operations related to traders')
+
+
+@ns_trader.route('/')
+class TraderCollection(Resource):
+
+    def __init__(self, api=None, *args, **kwargs):
+        super(TraderCollection, self).__init__(api, args, kwargs)
+
+    @api.marshal_list_with(trader_fields)
+    def get(self):
+        return Trader.query.all()
+
+    @api.expect(trader_fields)
+    @api.marshal_with(trader_fields, code=201)
+    def post(self):
+        trader = Trader(
+            first_name=api.payload['first_name'],
+            last_name=api.payload['last_name']
+        )
+        db.session.add(trader)
+        db.session.commit()
+
+        return trader
+
+
+@ns_trader.route('/<int:id>')
+@api.response(404, 'Trader not found.')
+class TraderItem(Resource):
+
+    def __init__(self, api=None, *args, **kwargs):
+        super(TraderItem, self).__init__(api, args, kwargs)
+
+    @api.marshal_with(trader_fields)
+    def get(self, id):
+        return Trader.query.filter(Trader.id == id).one()
+
+
+api.add_namespace(ns_trader)
