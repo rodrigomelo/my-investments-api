0e8bad84b72fd93a8c0a5d139aeca5a92f18f3af
Rodrigo Melo Lanna <melorodrigo@gmail.com>
2021-01-05 08:54:16 -0300

    Typo fix

--- application/routes.py
+++ application/routes.py
@@ -213,7 +213,7 @@ class CurrencyCollection(Resource):
             code=api.payload['code'],
             name=api.payload['name'],
             is_active=api.payload['is_active'],
-            is_base_currency=api.payload['nais_base_currencyme']
+            is_base_currency=api.payload['is_base_currency']
         )
         db.session.add(currency)
         db.session.commit()
