b8a538dbeb7898c93d65770a819d5ae6102d7123
Rodrigo Melo Lanna <melorodrigo@gmail.com>
2021-01-04 23:47:39 -0300

    Updated

--- application/models.py
+++ application/models.py
@@ -1,75 +1,160 @@
 from . import db
 
 
-class Trader(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(64), index=False, unique=False, nullable=False)
-    last_name = db.Column(db.String(64), index=False, unique=False, nullable=False)
-    # user_name = db.Column(db.String(64), index=False, unique=True, nullable=False)
-    # password = db.Column(db.String(64), index=False, unique=False, nullable=False)
-    email = db.Column(db.String(128), index=True, unique=True, nullable=True)
-    # confirmation_code = db.Column(db.String(128), index=False, unique=False, nullable=False)
-    # time_registered = db.Column(db.DateTime, index=False, unique=False, nullable=False)
-    # time_confirmed = db.Column(db.DateTime, index=False, unique=False, nullable=False)
-    # country_id = db.Column(db.Integer, index=False, unique=False, nullable=False)
-    # preferred_currency_id = db.Column(db.Integer, index=False, unique=False, nullable=False)
+class Country(db.Model):
+    # __tablename__ = 'country'
 
-    def __repr__(self):
-        return '<Trader first_name: {}>'.format(self.first_name)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True, unique=True, nullable=False)
+
+    def __init__(self, name):
+        self.name = name
 
 
 class Currency(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(8), index=True, unique=True, nullable=True)
-    name = db.Column(db.String(64), index=False, unique=False, nullable=True)
-
-
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(64), index=True, unique=True, nullable=False)
-    name = db.Column(db.String(255), index=False, unique=False, nullable=False)
-    trades = db.relationship('Trade', backref='item', lazy=True)
-
-
-class Trade(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
-
-
-class CurrentInventory(db.Model):
-    # __tablename__ = 'current_inventory'
+    # __tablename__ = 'currency'
 
     id = db.Column(db.Integer, primary_key=True)
-    trader_id_id = db.Column(db.Integer, db.ForeignKey('trader.id'), nullable=False)
-    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
-    quantity = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=False)
+    code = db.Column(db.String(8), index=True, unique=True, nullable=False) # e.g. iso code for currencies
+    name = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    is_active = db.Column(db.Boolean, index=False, unique=False, nullable=False)
+    is_base_currency = db.Column(db.Boolean, index=False, unique=False, nullable=False)
 
+    def __init__(self, code, name, is_active, is_base_currency):
+        self.code = code
+        self.name = name
+        self.is_active = is_active
+        self.is_base_currency = is_base_currency
 
-class Offer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-
-class Price(db.Model):
-    # __tablename__ = 'currency_rate'
-
-    id = db.Column(db.Integer, primary_key=True)
-
-
+# history rates between currencies (base currency and others)
 class CurrencyRate(db.Model):
     # __tablename__ = 'currency_rate'
 
     id = db.Column(db.Integer, primary_key=True)
-
-
-class Report(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-
-class Country(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    currency_id = db.Column(db.Integer, db.ForeignKey('currency.id'), nullable=False)
+    currency = db.relationship('Currency', backref='currency_rate', lazy=True, foreign_keys = [currency_id])
+    base_currency_id = db.Column(db.Integer, db.ForeignKey('currency.id'), nullable=False)
+    base_currency = db.relationship('Currency', backref='base_currency_rate', lazy=True, foreign_keys = [base_currency_id])
+    rate = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=False)
+    ts = db.Column(db.DateTime, index=False, unique=False, nullable=False)
 
 
 class CurrencyUsed(db.Model):
     # __tablename__ = 'currency_used'
 
     id = db.Column(db.Integer, primary_key=True)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'), nullable=False)
+    country = db.relationship('Country', backref='currency_used', lazy=True, foreign_keys = [country_id])
+    currency_id = db.Column(db.Integer, db.ForeignKey('currency.id'), nullable=False)
+    currency = db.relationship('Currency', backref='currency_used', lazy=True, foreign_keys = [currency_id])
+    date_from = db.Column(db.Date, index=False, unique=False, nullable=False)
+    date_to = db.Column(db.Date, index=False, unique=False, nullable=True)
+
+
+class CurrentInventory(db.Model):
+    # __tablename__ = 'current_inventory'
+
+    id = db.Column(db.Integer, primary_key=True)
+    trader_id = db.Column(db.Integer, db.ForeignKey('trader.id'), index=True, unique=True, nullable=False)
+    trader = db.relationship('Trader', backref='current_inventory', lazy=True, foreign_keys = [trader_id])
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), index=True, unique=True, nullable=False)
+    item = db.relationship('Item', backref='current_inventory', lazy=True, foreign_keys = [item_id])
+    quantity = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=False)
+
+
+class Item(db.Model):
+    # __tablename__ = 'item'
+
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    name = db.Column(db.String(255), index=False, unique=False, nullable=False)
+    is_active = db.Column(db.Boolean, index=False, unique=False, nullable=False)
+    currency_id = db.Column(db.Integer, db.ForeignKey('currency.id'), nullable=False)
+    currency = db.relationship('Currency', backref='item', lazy=True, foreign_keys = [currency_id])
+    details = db.Column(db.Text, index=False, unique=False, nullable=True)
+
+
+# list of all offers avaliable to buy/sell
+class Offer(db.Model):
+    # __tablename__ = 'offer'
+
+    id = db.Column(db.Integer, primary_key=True)
+    trader_id = db.Column(db.Integer, db.ForeignKey('trader.id'), index=True, unique=True, nullable=False)
+    trader = db.relationship('Trader', backref='offer', lazy=True, foreign_keys = [trader_id])
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+    item = db.relationship('Item', backref='offer', lazy=True, foreign_keys = [item_id])
+    quantity = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=False)
+    buy = db.Column(db.Boolean, index=False, unique=False, nullable=False)
+    sell = db.Column(db.Boolean, index=False, unique=False, nullable=False)
+    price = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=False)
+    ts = db.Column(db.DateTime, index=False, unique=False, nullable=False)
+    is_active = db.Column(db.Boolean, index=False, unique=False, nullable=False)
+
+
+# list of history prices (buy & sell)
+class Price(db.Model):
+    # __tablename__ = 'price'
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+    item = db.relationship('Item', backref='price', lazy=True, foreign_keys = [item_id])
+    currency_id = db.Column(db.Integer, db.ForeignKey('currency.id'), nullable=False)
+    currency = db.relationship('Currency', backref='price', lazy=True, foreign_keys = [currency_id])
+    buy = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=False)
+    sell = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=False)
+    ts = db.Column(db.DateTime, index=False, unique=False, nullable=False)
+
+
+class Report(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    trading_date = db.Column(db.Date, index=True, unique=True, nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), index=True, unique=True, nullable=False)
+    item = db.relationship('Item', backref='report', lazy=True, foreign_keys = [item_id])
+    currency_id = db.Column(db.Integer, db.ForeignKey('currency.id'), index=True, unique=True, nullable=False)
+    currency = db.relationship('Currency', backref='report', lazy=True, foreign_keys = [currency_id])
+    first_price = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=True)
+    last_price = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=True)
+    min_price = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=True)
+    max_price = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=True)
+    avg_price = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=True)
+    total_amount = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=True)
+    quantity = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=True)
+
+
+class Trade(db.Model):
+    # __tablename__ = 'trade'
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+    item = db.relationship('Item', backref='trade', lazy=True, foreign_keys = [item_id])
+    buyer_id = db.Column(db.Integer, db.ForeignKey('trader.id'), index=True, unique=True, nullable=False)
+    buyer = db.relationship('Trader', backref='trade_buyer', lazy=True, foreign_keys = [buyer_id])
+    seller_id = db.Column(db.Integer, db.ForeignKey('trader.id'), index=True, unique=True, nullable=True)
+    seller = db.relationship('Trader', backref='trade_seller', lazy=True, foreign_keys = [seller_id])
+    quantity = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=False)
+    unit_price = db.Column(db.Numeric(16,6), index=False, unique=False, nullable=False)
+    description = db.Column(db.Text, index=False, unique=False, nullable=False)
+    offer_id = db.Column(db.Integer, db.ForeignKey('offer.id'), nullable=False)
+    offer = db.relationship('Offer', backref='trade', lazy=True, foreign_keys = [offer_id])
+
+
+class Trader(db.Model):
+    # __tablename__ = 'trader'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(64), index=False, unique=False, nullable=False)
+    last_name = db.Column(db.String(64), index=False, unique=False, nullable=False)
+    user_name = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    password = db.Column(db.String(64), index=False, unique=False, nullable=False)
+    email = db.Column(db.String(128), index=True, unique=True, nullable=False)
+    confirmation_code = db.Column(db.String(128), index=False, unique=False, nullable=False)
+    time_registered = db.Column(db.DateTime, index=False, unique=False, nullable=False)
+    time_confirmed = db.Column(db.DateTime, index=False, unique=False, nullable=False)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'), nullable=False)
+    country = db.relationship('Country', backref='trader', lazy=True, foreign_keys = [country_id])
+    preferred_currency_id = db.Column(db.Integer, db.ForeignKey('currency.id'), nullable=False)
+    preferred_currency = db.relationship('Currency', backref='trader', lazy=True, foreign_keys = [preferred_currency_id])
+
+    def __repr__(self):
+        return '<Trader user_name: {}>'.format(self.user_name)
+
--- application/routes.py
+++ application/routes.py
@@ -5,29 +5,34 @@ from flask import current_app as app
 from flask_restplus import Api, Resource, fields
 from sqlalchemy.orm.exc import NoResultFound
 from . import db
-from .models import Trader, Currency, Item, Trade, CurrentInventory, Offer, Price, CurrencyRate, Report, Country, CurrencyUsed
-
-api = Api(version='1.0', title='my-investments-api')
+from .models import Country, Currency, CurrencyRate, CurrencyUsed, Trader, Item, CurrentInventory, Offer, Price, Report, Trade
 
 blueprint = Blueprint('api', __name__, url_prefix='/api')
 
-api.init_app(blueprint)
+api = Api(blueprint, doc='/doc/')
+
 app.register_blueprint(blueprint)
 
 ns_default = api.default_namespace
 api.add_namespace(ns_default)
 
-ns_trader = api.namespace('traders')
-api.add_namespace(ns_trader)
+ns_country = api.namespace('countries')
+api.add_namespace(ns_country)
 
 ns_currency = api.namespace('currencies')
 api.add_namespace(ns_currency)
 
+ns_currency_rate = api.namespace('currency_rates')
+api.add_namespace(ns_currency_rate)
+
+ns_currency_used = api.namespace('currencies_used')
+api.add_namespace(ns_currency_used)
+
+ns_trader = api.namespace('traders')
+api.add_namespace(ns_trader)
+
 ns_item = api.namespace('items')
 api.add_namespace(ns_item)
-
-ns_trade = api.namespace('trades')
-api.add_namespace(ns_trade)
 
 ns_current_inventory = api.namespace('current_inventories')
 api.add_namespace(ns_current_inventory)
@@ -38,67 +43,115 @@ api.add_namespace(ns_offer)
 ns_price = api.namespace('prices')
 api.add_namespace(ns_price)
 
-ns_currency_rate = api.namespace('currency_rates')
-api.add_namespace(ns_currency_rate)
-
 ns_report = api.namespace('reports')
 api.add_namespace(ns_report)
 
-ns_country = api.namespace('countries')
-api.add_namespace(ns_country)
+ns_trade = api.namespace('trades')
+api.add_namespace(ns_trade)
 
-ns_currency_used = api.namespace('currencies_used')
-api.add_namespace(ns_currency_used)
-
-trader_fields = api.model('Trader', {
+country_fields = api.model('Country', {
     'id': fields.Integer(readonly=True),
-    'first_name': fields.String(required=True),
-    'last_name': fields.String(required=True),
-    'email': fields.String(required=True)
+    'name': fields.String(required=True)
 })
 
 currency_fields = api.model('Currency', {
     'id': fields.Integer(readonly=True),
     'code': fields.String(required=True),
-    'name': fields.String(required=True)
+    'name': fields.String(required=True),
+    'is_active': fields.Boolean(required=True),
+    'is_base_currency': fields.Boolean(required=True)
+})
+
+currency_rate_fields = api.model('CurrencyRate', {
+    'id': fields.Integer(readonly=True),
+    'currency': fields.Nested(currency_fields, required=True),
+    'base_currency': fields.Nested(currency_fields, required=True),
+    'rate': fields.Float(required=True),
+    'ts': fields.DateTime(required=True)
+})
+
+currency_used_fields = api.model('CurrencyUsed', {
+    'id': fields.Integer(readonly=True),
+    'country': fields.Nested(country_fields, required=True),
+    'currency': fields.Nested(currency_fields, required=True),
+    'date_from': fields.Date(required=True),
+    'date_to': fields.Date(required=False)
+})
+
+trader_fields = api.model('Trader', {
+    'id': fields.Integer(readonly=True),
+    'first_name': fields.String(required=True),
+    'last_name': fields.String(required=True),
+    'user_name': fields.String(required=True),
+    'password': fields.String(required=True),
+    'email': fields.String(required=True),
+    'confirmation_code': fields.String(required=True),
+    'time_registered': fields.DateTime(required=True),
+    'time_confirmed': fields.DateTime(required=True),
+    'country': fields.Nested(country_fields, required=True),
+    'preferred_currency': fields.Nested(currency_fields, required=True)
 })
 
 item_fields = api.model('Item', {
     'id': fields.Integer(readonly=True),
     'code': fields.String(required=True),
-    'name': fields.String(required=True)
-})
-
-trade_fields = api.model('Trade', {
-    'id': fields.Integer(readonly=True)
+    'name': fields.String(required=True),
+    'is_active': fields.Boolean(required=True),
+    'currency': fields.Nested(currency_fields, required=True),
+    'details': fields.String(required=False)
 })
 
 current_inventory_fields = api.model('CurrentInventory', {
-    'id': fields.Integer(readonly=True)
+    'id': fields.Integer(readonly=True),
+    'trader': fields.Nested(trader_fields, required=True),
+    'item': fields.Nested(item_fields, required=True),
+    'quantity': fields.Float(required=True)
 })
 
 offer_fields = api.model('Offer', {
-    'id': fields.Integer(readonly=True)
+    'id': fields.Integer(readonly=True),
+    'trader': fields.Nested(trader_fields, required=True),
+    'item': fields.Nested(item_fields, required=True),
+    'quantity': fields.Float(required=True),
+    'buy': fields.Boolean(required=True),
+    'sell': fields.Boolean(required=True),
+    'price': fields.Float(required=False),
+    'ts': fields.DateTime(required=True),
+    'is_active': fields.Boolean(required=True)
 })
 
 price_fields = api.model('Price', {
-    'id': fields.Integer(readonly=True)
-})
-
-currency_rate_fields = api.model('CurrencyRate', {
-    'id': fields.Integer(readonly=True)
+    'id': fields.Integer(readonly=True),
+    'item': fields.Nested(item_fields, required=True),
+    'currency': fields.Nested(currency_fields, required=True),
+    'buy': fields.Float(required=True),
+    'sell': fields.Float(required=True),
+    'ts': fields.DateTime(required=True)
 })
 
 report_fields = api.model('Report', {
-    'id': fields.Integer(readonly=True)
+    'id': fields.Integer(readonly=True),
+    'trading_date': fields.Date(required=True),
+    'item': fields.Nested(item_fields, required=True),
+    'currency': fields.Nested(currency_fields, required=True),
+    'first_price': fields.Float(required=False),
+    'last_price': fields.Float(required=False),
+    'min_price': fields.Float(required=False),
+    'max_price': fields.Float(required=False),
+    'avg_price': fields.Float(required=False),
+    'total_amount': fields.Float(required=False),
+    'quantity': fields.Float(required=False)
 })
 
-country_fields = api.model('Country', {
-    'id': fields.Integer(readonly=True)
-})
-
-currency_used_fields = api.model('CurrencyUsed', {
-    'id': fields.Integer(readonly=True)
+trade_fields = api.model('Trade', {
+    'id': fields.Integer(readonly=True),
+    'item': fields.Nested(item_fields),
+    'buyer': fields.Nested(trader_fields),
+    'seller': fields.Nested(trader_fields, required=False),
+    'quantity': fields.Float(required=True),
+    'unit_price': fields.Float(required=True),
+    'description': fields.String(required=True),
+    'offer': fields.Nested(offer_fields, required=True)
 })
 
 
@@ -119,6 +172,96 @@ def database_not_found_error_handler(e):
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
+
+@ns_country.route('/')
+class CountryCollection(Resource):
+
+    def __init__(self, api=None, *args, **kwargs):
+        super(CountryCollection, self).__init__(api, args, kwargs)
+
+    @api.marshal_list_with(country_fields)
+    def get(self):
+        return Country.query.all()
+
+    @api.expect(country_fields)
+    @api.marshal_with(country_fields, code=201)
+    def post(self):
+        country = Country(
+            name = api.payload['name']
+        )
+        db.session.add(country)
+        db.session.commit()
+
+        return country
+
+
+@ns_currency.route('/')
+class CurrencyCollection(Resource):
+
+    def __init__(self, api=None, *args, **kwargs):
+        super(CurrencyCollection, self).__init__(api, args, kwargs)
+
+    @api.marshal_list_with(currency_fields)
+    def get(self):
+        return Currency.query.all()
+
+    @api.expect(currency_fields)
+    @api.marshal_with(currency_fields, code=201)
+    def post(self):
+        currency = Currency(
+            code=api.payload['code'],
+            name=api.payload['name'],
+            is_active=api.payload['is_active'],
+            is_base_currency=api.payload['nais_base_currencyme']
+        )
+        db.session.add(currency)
+        db.session.commit()
+
+        return currency
+
+
+@ns_currency_rate.route('/')
+class CurrencyRateCollection(Resource):
+
+    def __init__(self, api=None, *args, **kwargs):
+        super(CurrencyRateCollection, self).__init__(api, args, kwargs)
+
+    @api.marshal_list_with(currency_rate_fields)
+    def get(self):
+        return CurrencyRate.query.all()
+
+    @api.expect(currency_rate_fields)
+    @api.marshal_with(currency_rate_fields, code=201)
+    def post(self):
+        currency_rate = CurrencyRate(
+        )
+        db.session.add(currency_rate)
+        db.session.commit()
+
+        return currency_rate
+
+
+@ns_currency_used.route('/')
+class CurrencyUsedCollection(Resource):
+
+    def __init__(self, api=None, *args, **kwargs):
+        super(CurrencyUsedCollection, self).__init__(api, args, kwargs)
+
+    @api.marshal_list_with(currency_used_fields)
+    def get(self):
+        return CurrencyUsed.query.all()
+
+    @api.expect(currency_used_fields)
+    @api.marshal_with(currency_used_fields, code=201)
+    def post(self):
+        currency_used = CurrencyUsed(
+        )
+        db.session.add(currency_used)
+        db.session.commit()
+
+        return currency_used
+
 
 
 @ns_trader.route('/')
@@ -157,28 +300,6 @@ class TraderItem(Resource):
         return Trader.query.filter(Trader.id == id).one()
 
 
-@ns_currency.route('/')
-class CurrencyCollection(Resource):
-
-    def __init__(self, api=None, *args, **kwargs):
-        super(CurrencyCollection, self).__init__(api, args, kwargs)
-
-    @api.marshal_list_with(currency_fields)
-    def get(self):
-        return Currency.query.all()
-
-    @api.expect(currency_fields)
-    @api.marshal_with(currency_fields, code=201)
-    def post(self):
-        currency = Currency(
-            code=api.payload['code']
-        )
-        db.session.add(currency)
-        db.session.commit()
-
-        return currency
-
-
 @ns_item.route('/')
 class ItemCollection(Resource):
 
@@ -200,27 +321,6 @@ class ItemCollection(Resource):
         db.session.commit()
 
         return item
-
-
-@ns_trade.route('/')
-class TradeCollection(Resource):
-
-    def __init__(self, api=None, *args, **kwargs):
-        super(TradeCollection, self).__init__(api, args, kwargs)
-
-    @api.marshal_list_with(trade_fields)
-    def get(self):
-        return Trade.query.all()
-
-    @api.expect(trade_fields)
-    @api.marshal_with(trade_fields, code=201)
-    def post(self):
-        trade = Trade(
-        )
-        db.session.add(trade)
-        db.session.commit()
-
-        return trade
 
 
 @ns_current_inventory.route('/')
@@ -286,27 +386,6 @@ class PriceCollection(Resource):
         return price
 
 
-@ns_currency_rate.route('/')
-class CurrencyRateCollection(Resource):
-
-    def __init__(self, api=None, *args, **kwargs):
-        super(CurrencyRateCollection, self).__init__(api, args, kwargs)
-
-    @api.marshal_list_with(currency_rate_fields)
-    def get(self):
-        return CurrencyRate.query.all()
-
-    @api.expect(currency_rate_fields)
-    @api.marshal_with(currency_rate_fields, code=201)
-    def post(self):
-        currency_rate = CurrencyRate(
-        )
-        db.session.add(currency_rate)
-        db.session.commit()
-
-        return currency_rate
-
-
 @ns_report.route('/')
 class ReportCollection(Resource):
 
@@ -328,44 +407,22 @@ class ReportCollection(Resource):
         return report
 
 
-@ns_country.route('/')
-class CountryCollection(Resource):
+@ns_trade.route('/')
+class TradeCollection(Resource):
 
     def __init__(self, api=None, *args, **kwargs):
-        super(CountryCollection, self).__init__(api, args, kwargs)
+        super(TradeCollection, self).__init__(api, args, kwargs)
 
-    @api.marshal_list_with(country_fields)
+    @api.marshal_list_with(trade_fields)
     def get(self):
-        return Country.query.all()
+        return Trade.query.all()
 
-    @api.expect(country_fields)
-    @api.marshal_with(country_fields, code=201)
+    @api.expect(trade_fields)
+    @api.marshal_with(trade_fields, code=201)
     def post(self):
-        country = Country(
+        trade = Trade(
         )
-        db.session.add(country)
+        db.session.add(trade)
         db.session.commit()
 
-        return country
-
-
-@ns_currency_used.route('/')
-class CurrencyUsedCollection(Resource):
-
-    def __init__(self, api=None, *args, **kwargs):
-        super(CurrencyUsedCollection, self).__init__(api, args, kwargs)
-
-    @api.marshal_list_with(currency_used_fields)
-    def get(self):
-        return CurrencyUsed.query.all()
-
-    @api.expect(currency_used_fields)
-    @api.marshal_with(currency_used_fields, code=201)
-    def post(self):
-        currency_used = CurrencyUsed(
-        )
-        db.session.add(currency_used)
-        db.session.commit()
-
-        return currency_used
-
+        return trade
